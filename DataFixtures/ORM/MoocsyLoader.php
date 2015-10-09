f2382647c5e498924f9c6b45de3f9c399da6f2c1
Cristian Angulo Nova <cristianangulonova@gmail.com>
2015-10-09 17:04:59 -0500

    AliceBundle

--- DataFixtures/ORM/MoocsyLoader.php
+++ DataFixtures/ORM/MoocsyLoader.php
@@ -6,9 +6,16 @@ use Hautelook\AliceBundle\Doctrine\DataFixtures\AbstractLoader;
 
 class MoocsyLoader extends AbstractLoader
 {
-    /**
-     * {@inheritdoc}
-     */
+
+    protected $modules;
+    protected $items;
+
+    public function __construct()
+    {
+        $this->modules = 21;
+        $this->items   = 105;
+    }
+
     public function getFixtures()
     {
         return [
@@ -19,5 +26,51 @@ class MoocsyLoader extends AbstractLoader
     public function published()
     {
         return new \Datetime('now');
+    }
+
+    public function module()
+    {
+        $modules = [];
+        $module = 'module';
+
+        for ($i=0; $i < $this->modules; $i++) {
+            $modules[$i] = $module.$i;
+        }
+
+        return $modules[array_rand($modules)];
+    }
+
+    public function items()
+    {
+        $items = [];
+        $item = 'item';
+
+        for ($i=0; $i < $this->items; $i++) {
+            $items[$i] = $item.$i;
+        }
+
+        return $items[array_rand($items)];
+    }
+
+    public function modules()
+    {
+
+        return rand(0, $this->modules);
+    }
+
+    public function itemsType()
+    {
+        $itemValue = rand(0, 5);
+
+        $itemsType = array(
+            'items_audio',
+            'items_audio_down',
+            'items_file',
+            'items_forum',
+            'items_video',
+            'items_quiz'
+        );
+
+        return $itemsType[$itemValue];
     }
 }
